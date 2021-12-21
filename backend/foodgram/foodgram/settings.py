3366382e0d3ff14c76965f16af4992c545001b36
fibboo <fibboo@gmail.com>
2021-12-22 00:47:01 +0300

    Fix djoser user permissions

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -139,7 +139,7 @@ DJOSER = {
         'current_user': 'users.serializers.CustomUserSerializer',
     },
     'PERMISSIONS': {
-        'user:': ['rest_framework.permissions.AllowAny'],
+        'user': ['rest_framework.permissions.IsAuthenticated'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
     'HIDE_USERS': False
