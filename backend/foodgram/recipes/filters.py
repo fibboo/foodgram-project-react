ba63eb60130cdbf2c7f8b0ebcc1579624bfcf7b2
fibboo <fibboo@gmail.com>
2021-12-28 23:29:56 +0300

    Stuck with filters for is_favorited and is_in_shopping_cart

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -5,7 +5,8 @@ from .models import Recipe
 
 class RecipeFilter(django_filters.FilterSet):
     tags = django_filters.CharFilter(field_name='tags__slug')
-    # is_favorited = django_filters.NumberFilter()
+    # is_favorited = django_filters.BooleanFilter(field_name='favorite_recipe')
+    # is_in_shopping_cart = django_filters.BooleanFilter()
 
     class Meta:
         model = Recipe
